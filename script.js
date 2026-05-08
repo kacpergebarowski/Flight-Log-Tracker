23f9c349f33e34d56c93cfea1547e574054803b7
kacpergebarowski <kacperjgebarowski@lewisu.edu>
2026-05-07 19:47:01 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function addFlight() {
+  const date = document.getElementById("date").value;
+  const aircraft = document.getElementById("aircraft").value;
+  const route = document.getElementById("route").value;
+  const hours = document.getElementById("hours").value;
+
+  console.log(date);
+  console.log(aircraft);
+  console.log(route);
+  console.log(hours);
+}
