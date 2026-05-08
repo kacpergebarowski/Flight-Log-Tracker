1824daf83b1323f766245da0f3cf23d48d0626e6
kacpergebarowski <kacperjgebarowski@lewisu.edu>
2026-05-07 20:05:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+let total = 0;
+
 function addFlight() {
+
   const date = document.getElementById("date").value;
   const aircraft = document.getElementById("aircraft").value;
   const route = document.getElementById("route").value;
   const hours = document.getElementById("hours").value;
 
-  console.log(date);
-  console.log(aircraft);
-  console.log(route);
-  console.log(hours);
+  const table = document.getElementById("flightTable");
+
+  const newRow = table.insertRow();
+
+  newRow.insertCell(0).innerHTML = date;
+  newRow.insertCell(1).innerHTML = aircraft;
+  newRow.insertCell(2).innerHTML = route;
+  newRow.insertCell(3).innerHTML = hours;
+
+  total = total + parseFloat(hours);
+
+  document.getElementById("totalHours").innerHTML = total;
+
 }
